e5f0fe0337b0dae48fbfbaba3c55705be2899509
Ditto Head <uasanya1927@gmail.com>
2023-01-26 21:12:36 +0200

    added backstack

--- app/src/main/java/com/example/srez/ui/base/BaseFragment.java
+++ app/src/main/java/com/example/srez/ui/base/BaseFragment.java
@@ -14,20 +14,23 @@ import com.example.srez.R;
 public abstract class BaseFragment extends Fragment {
 
     private FragmentManager fm;
-    private FragmentTransaction ft;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         fm = requireActivity().getSupportFragmentManager();
-        ft = fm.beginTransaction();
     }
 
     protected void navigate(Fragment fragment) {
-        ft.replace(R.id.container, fragment).commit();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.container, fragment,fragment.getTag()).addToBackStack(null).commit();
     }
 
     protected void showToast(@StringRes Integer res) {
         Toast.makeText(getActivity(), res, Toast.LENGTH_LONG).show();
+    }
+
+    protected void navigateBack(){
+        requireActivity().onBackPressed();
     }
 }
